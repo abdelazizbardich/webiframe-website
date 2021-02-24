82561cbe41f139614fe53631b2d33a10c7f2437c
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-02-24 17:54:28 +0100

    Auto scroll using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
     // header scroll controller
+    if($(window).scrollTop() > 70){$("header").addClass("bg-w-70");}
     $(window).scroll(function () {
         let currentScroll = $(window).scrollTop();
         if(currentScroll > 70){
             $("header").addClass("bg-w-70");
         }else{
             $("header").removeClass("bg-w-70");
+        }
+    });
+
+    // auto scroll to section
+    $("a").click(function(e){
+        let target = $(this).attr('href');
+        if(target.includes("#")){
+            e.preventDefault();
+            console.log($(target));
+            $('html, body').animate({
+                scrollTop: $(target).offset().top-100
+            },500);
         }
     });
 
