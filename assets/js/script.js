4788a4ebd2cf7e091e577e94e4c5553c34eca4dc
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-02-24 14:01:28 +0100

    creation et implementation du nav menu pour le mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // header scroll controller
     $(window).scroll(function () {
         let currentScroll = $(window).scrollTop();
         if(currentScroll > 70){
@@ -7,4 +8,24 @@ $(document).ready(function(){
             $("header").removeClass("bg-w-70");
         }
     });
+
+    // Header search bar cntroller
+    $('.header-s i').click(function () {
+       $('.s-box').toggleClass("show"); 
+    });
+    $('main').click(function () {
+        $('.s-box').removeClass("show"); 
+     });
+
+    // Show mobile nav
+    $('.show-m-nav').click(function () {
+        $(this).toggleClass('open');
+        $('.nav').toggleClass('show');
+    });
+    // nav links manager
+    $('.nav li').click(function(){
+        $('.show-m-nav').toggleClass('open');
+        $(this).parents('.nav').toggleClass('show');
+    });
+
 });
