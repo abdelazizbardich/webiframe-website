fca05764d75c72f1dcac1914984a4eba1a68084f
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2026-07-04 09:41:40 +0100

    Fix environment and debug configuration values in app.php

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
+    'env' => env('production', 'production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env(false, false),
 
     /*
     |--------------------------------------------------------------------------
